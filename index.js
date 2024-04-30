791f77187b68d8f1d164d29451f09c07350b7b57
+ <+>
2024-04-30 12:10:06 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+const formData = {
+    name: "Byron",
+    email: "byronb@gamil.com",
+  };
+  
+  const submitData = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/users", submitData)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+        alert("Bad things!");
+        console.log(error.message);
+      });
+
+return fetch(submitData);
